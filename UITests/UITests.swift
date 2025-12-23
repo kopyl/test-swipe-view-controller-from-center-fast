07d747e3e9f44eaa2242905f8d3c6c3027ea321c
kopyl <oleggggg1997@gmail.com>
2025-12-23 19:27:13 +0200

    Add push test without a glitch

--- UITests/UITests.swift
+++ UITests/UITests.swift
@@ -1,5 +1,23 @@
 import XCTest
 
+func preparePushVCTest() -> (XCUICoordinate, XCUICoordinate) {
+    let app = XCUIApplication()
+    app.launch()
+
+    let pushButton = app.buttons["Push VC"]
+    XCTAssertTrue(pushButton.waitForExistence(timeout: 5))
+    pushButton.tap()
+    
+    XCTAssertTrue(pushButton.waitForNonExistence(timeout: 5))
+    
+    let window = app.windows.firstMatch
+    let startPoint = window.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5))
+    let endPoint = window.coordinate(withNormalizedOffset: CGVector(dx: 0.6, dy: 0.5))
+    
+    return (startPoint, endPoint)
+}
+
+
 final class UITests: XCTestCase {
 
     override func setUpWithError() throws {
@@ -7,19 +25,14 @@ final class UITests: XCTestCase {
     }
 
     @MainActor
-    func testPushViewControllerAndSwipeBack() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        let pushButton = app.buttons["Push VC"]
-        XCTAssertTrue(pushButton.waitForExistence(timeout: 5))
-        pushButton.tap()
-        
-        XCTAssertTrue(pushButton.waitForNonExistence(timeout: 5))
-        
-        let window = app.windows.firstMatch
-        let startPoint = window.coordinate(withNormalizedOffset: CGVector(dx: 0.5, dy: 0.5))
-        let endPoint = window.coordinate(withNormalizedOffset: CGVector(dx: 0.6, dy: 0.5))
+    func testPushViewControllerAndSwipeBackWithGlitch() throws {
+        let (startPoint, endPoint) = preparePushVCTest()
         startPoint.press(forDuration: 0.05, thenDragTo: endPoint, withVelocity: XCUIGestureVelocity(3000), thenHoldForDuration: 0)
+    }
+    
+    @MainActor
+    func testPushViewControllerAndSwipeBackNoGlitch() throws {
+        let (startPoint, endPoint) = preparePushVCTest()
+        startPoint.press(forDuration: 0.05, thenDragTo: endPoint, withVelocity: .fast, thenHoldForDuration: 0)
     }
 }
